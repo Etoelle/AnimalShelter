4a1a3c57f637f5fa3e9be5ab9cb99acd0236db62
Lara <laralauro@outlook.com>
2024-06-14 12:03:41 +0200

    First page created

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    colors: {
+      cornsilk: "#FFF8DC",
+      offwhite: "#FAF9F6",
+      orange: "#FFA95D",
+      orangetransparent: "#ffa95d77",
+      tomato: "#F65D46",
+      tomatotransparent: "#f65e4667",
+      plum: "#3B1D2C",
+      plumer: "#7a3c5b ",
+    },
+  },
+};
